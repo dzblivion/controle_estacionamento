9e422771181eab7b5c92cc553b164cd1ed655fc9
deyvideuk <deyvideukcosta.dc@gmail.com>
2025-11-20 12:19:08 -0300

    Feat: criação do home refactor: organização das pastas e arquivos do projeto!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { SobreNos } from './pages/sobre-nos/sobre-nos';
+import { Home } from './componentes/paginas/home/home';
+
 
 export const routes: Routes = [
+    { path: '', component: Home},
     { path: 'sobre_nos', component: SobreNos}
 ];
