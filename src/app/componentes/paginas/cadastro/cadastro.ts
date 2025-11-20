66343b16ddca0f4d3befb08f479ea2313b5b52a5
deyvideuk <deyvideukcosta.dc@gmail.com>
2025-11-20 18:59:24 -0300

    refactor: correção de formatação do input telefone!

--- src/app/componentes/paginas/cadastro/cadastro.ts
+++ src/app/componentes/paginas/cadastro/cadastro.ts
@@ -16,7 +16,7 @@ export class Cadastro {
   cadastro = new FormGroup({
       nome : new FormControl('', Validators.required),
       email : new FormControl('', [Validators.required, Validators.email]),
-      telefone : new FormControl(null, [Validators.required, Validators.pattern(/^\(\d{2}\)\s\d{4,5}-?\d{4}$/)])
+      telefone : new FormControl<string>('', [Validators.required, Validators.pattern(/^\(\d{2}\)\s\d{4,5}-?\d{4}$/)])
 
     });
 
@@ -25,4 +25,26 @@ export class Cadastro {
         alert("cadastro efetuado para:" + " \n" + "______________________\n" + this.cadastro.value.nome + "\n" + this.cadastro.value.email + "\n" + this.cadastro.value.telefone + "\n______________________\n" + "\n Enviar para o banco de dados!");
       }
     }
+
+    formatar() {
+    const control = this.cadastro.get('telefone');
+    let valor = control?.value || '';
+    valor = valor.replace(/\D/g, '');
+
+    valor = valor.substring(0, 11);
+
+    if (valor.length > 6) {
+      // (XX) XXXXX-XXXX
+      valor = valor.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+    } else if (valor.length > 2) {
+      // (XX) XXXXX
+      valor = valor.replace(/(\d{2})(\d+)/, '($1) $2');
+    } else if (valor.length > 0) {
+      // (XX
+      valor = valor.replace(/(\d{0,2})/, '($1');
+    }
+
+    control?.setValue(valor, { emitEvent: false });
+  }
+
 }
