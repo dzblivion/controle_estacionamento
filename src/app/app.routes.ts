0510df46ac49fd765a15069578069e20f4d5c7c7
deyvideuk <deyvideukcosta.dc@gmail.com>
2025-11-27 12:07:45 -0300

    feat: página de login criada!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { SobreNos } from './pages/sobre-nos/sobre-nos';
 import { Cadastro } from './componentes/paginas/cadastro/cadastro';
+import { Login } from './componentes/paginas/login/login';
 
 
 export const routes: Routes = [
     { path: 'cadastro', component: Cadastro},
-    { path: 'sobre_nos', component: SobreNos}
+    { path: 'sobre_nos', component: SobreNos},
+    { path: "login", component: Login }
 ];
