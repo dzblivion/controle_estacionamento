16a1f2bbb1b6d336553716ee1ae28910c8fd12a7
deyvideuk <deyvideukcosta.dc@gmail.com>
2025-12-03 16:31:27 -0300

    feat: SQL da tabela usuarios e veiculos!

---
+++ backend/estacionamento.sql
@@ -0,0 +1,23 @@
+
+DROP TABLE IF EXISTS `usuarios`;
+
+CREATE TABLE `usuarios` (
+  `idUsuario` int NOT NULL AUTO_INCREMENT,
+  `nomeUsuario` varchar(45) NOT NULL,
+  `emailUsuario` varchar(50) NOT NULL,
+  `categoriaUsuario` varchar(45) DEFAULT NULL,
+  `statusUsuario` varchar(45) DEFAULT NULL,
+  `senhaUsuario` varchar(255) NOT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `veiculos`;
+
+CREATE TABLE `veiculos` (
+  `idVeiculo` int NOT NULL,
+  `placaVeiculo` varchar(45) NOT NULL,
+  `idUsuario` int NOT NULL,
+  `statusVeiculo` varchar(45) NOT NULL,
+  PRIMARY KEY (`idVeiculo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
